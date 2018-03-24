121f461316a4cac63f901b9ebc3b532ea04e0a98
Niels Willig <niels.willig@rwth-aachen.de>
2018-03-24 21:48:43 +0100

    Removed TestCase

--- core/src/test/java/com/shekhar/ce/core/TestCalculator.java
+++ core/src/test/java/com/shekhar/ce/core/TestCalculator.java
@@ -9,19 +9,4 @@ public class TestCalculator {
     public void testAdd() {
         Assert.assertEquals(2, new Calculator().add(1,1));
     }
-
-   /* @Test
-    public void testSub() {
-        Assert.assertEquals(0, new Calculator().sub(1,1));
-    }
-
-    @Test
-    public void testMult() {
-        Assert.assertEquals(4, new Calculator().mul(2,2));
-    }
-
-    @Test
-    public void testDiv() {
-        Assert.assertEquals(2, new Calculator().div(2,1));
-    }*/
 }
